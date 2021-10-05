8cdc732a4b2fd9e1c29b8a0847d07f19c99b2393
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-05 14:56:29 +0200

    Add files via upload

--- task 10.py
+++ task 10.py
@@ -2,8 +2,8 @@ def commonletter():
   firstword = input("Enter a first word: ")
   secondword = input("Enter a second word: ")
   wordscheck=list(set(firstword)&set(secondword))
-  for i in wordscheck:
-    print(i)
+  for letter in wordscheck:
+    print(letter)
 commonletter()
 
 
