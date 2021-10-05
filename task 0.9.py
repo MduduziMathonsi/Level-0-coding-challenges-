65e3be754336df80a85e47a2c26f3f18f9c7c02d
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-05 15:55:08 +0200

    Add files via upload

--- task 0.9.py
+++ task 0.9.py
@@ -1,8 +1,9 @@
 def vowels():
  word=input("Enter word: ")
  word = str.lower(word)
+ vowel= ('a', 'e', 'i', 'o', 'u')
  for letter in word:
-  if letter in "aAeEiIoOuU":
+  if letter in vowel:
     print(letter)
     
 vowels()
