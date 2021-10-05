be9c03f5bd7e68f5743fd62e9acf8237f7dfad8b
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-05 14:19:09 +0200

    Add files via upload

--- task 0.9.py
+++ task 0.9.py
@@ -1,6 +1,9 @@
-word = "Umuzi"
-for letter in word:
-  if letter in "aeiou":
+def vowels():
+ word=input("Enter word: ")
+ word = str.lower(word)
+ for letter in word:
+  if letter in "aAeEiIoOuU":
     print(letter)
-
+    
+vowels()
     
