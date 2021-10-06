bee63f06736c0e51986d495e20f73efb491589c9
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-06 11:49:27 +0200

    Add files via upload

--- task 0.9.py
+++ task 0.9.py
@@ -1,6 +1,8 @@
 def vowels():
  word=input("Enter word: ")
  word = str.lower(word)
+ #My logic can be wrong here as it print vowels as long as the finds one eg umuzi U will be printed twice 
+ #not sure how to avoid this but working on it.
  vowel= ('a', 'e', 'i', 'o', 'u')
  for letter in word:
   if letter in vowel:
