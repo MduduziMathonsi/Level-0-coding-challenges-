59cf16740f6244984cad544a608af381475ffa63
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-06 13:52:16 +0200

    Add files via upload

---
+++ task 0.8.py
@@ -0,0 +1,11 @@
+def hoursminutes():
+ number = input("Enter number :")
+ convnumber = int(number)
+ hours= int(convnumber / 60)
+ minutes = int(convnumber-(1*60))
+ print(hours ,":" ,minutes)
+hoursminutes()
+
+
+
+
