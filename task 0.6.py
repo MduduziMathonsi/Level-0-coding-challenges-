900e7cef7263da8413a2b138ba7e8787ccf19302
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-06 23:39:05 +0200

    Add files via upload

--- task 0.6.py
+++ task 0.6.py
@@ -1,7 +1,4 @@
-def maximum():
- num1 = input("Enter the first number :")
- num2 = input("Enter the second number :")
- num3 = input("Enter the third number :")
+def maximum(num1,num2,num3):
  if num1 > num2 and num1 > num3 :
    print(num1)
  elif num2 > num1 and num2 > num3 :
@@ -12,4 +9,4 @@ def maximum():
      #if user entered two or more same numbers and it happens to be the highest numbers
      print("Two or more number/s are equal and are maximum numbers") 
 
-maximum()
+maximum(10,34,50)
