cec861d26a13f0ef1a805bbc1e2e26480194f0be
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-07 10:28:59 +0200

    Add files via upload

--- task 0.8.py
+++ task 0.8.py
@@ -1,9 +1,14 @@
 def hoursminutes():
- number = input("Enter number :")
+ number = 61
  convnumber = int(number)
- hours= int(convnumber / 60)
- minutes = int(convnumber-(1*60))
- print(hours ,":" ,minutes)
+ h= int(convnumber / 60)
+ m = int(convnumber-(1*60))
+ if h<=1 and m<=1 :
+  print(h,"hour :",m,"minute")
+ elif h>=1 and m>=1 :
+  print(h,"hours :",m,"minutes")
+ else:
+   print("Abort Mission Impossible :-)")
 hoursminutes()
 
 
