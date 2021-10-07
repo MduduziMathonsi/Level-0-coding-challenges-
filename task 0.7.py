a53c5006c813e865d30cf3d9ca75ec5aeaf9d66a
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-07 14:00:58 +0200

    Add files via upload

--- task 0.7.py
+++ task 0.7.py
@@ -1,14 +1,12 @@
-#celsius function
-def tempcelsius():
- fahrenheit = float(79)
+######celsius function#####
+def tempcelsius(fahrenheit):
  celsius=(fahrenheit-32)*5/9
  print("Temperature in Celsius :",round(celsius,0))
-tempcelsius()
+tempcelsius(79)
 
-#fahrenheit function
-def tempfahrenheit ():
- celsius = float(26)
+######fahrenheit function#####
+def tempfahrenheit(celsius):
  fahrenheit =(celsius*9/5)+ 32
  print("Temperature in Fahrenheit :",round(fahrenheit,0))
-tempfahrenheit()
+tempfahrenheit(26)
 
