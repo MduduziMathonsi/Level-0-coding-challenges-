62d25cac4ffaf822e4f4e60e5c02026fb7d692d4
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-07 13:55:56 +0200

    Add files via upload

--- task 0.6.py
+++ task 0.6.py
@@ -1,15 +1,12 @@
-def maximum():
- num1 = int(10)
- num2 = int(20)
- num3 = int(15)
+def maximum(num1,num2,num3):
  if num1 > num2 and num1 > num3 :
-   print(num1)
+   print("Highest Number:",num1)
  elif num2 > num1 and num2 > num3 :
-     print(num2)
+     print("Highest Number:",num2)
  elif num3 > num1 and num3 > num2 :
-     print(num3)
+     print("Highest Number:",num3)
  else:
      #if two or more same numbers happens to be the highest numbers
      print("Two or more number/s are equal and are maximum numbers") 
 
-maximum()
+maximum(10,30,15)
