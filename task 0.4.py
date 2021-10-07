86b01a433ae643ad3314e02fe7d7524423f13451
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-07 13:50:20 +0200

    Add files via upload

--- task 0.4.py
+++ task 0.4.py
@@ -1,9 +1,8 @@
-def evenOrOdd():
- number =int(8)
+def evenOrOdd(number): #should not ask the user for input hence function parameter 
  check = number % 2 
  if check > 0:
     print("odd")
  else:
     print("even")
-evenOrOdd()
+evenOrOdd(77) #function to print odd or even hence called a function with an arg
     
