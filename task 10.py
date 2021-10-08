4e757bf11cd03052e951c9917bbfe20f6a24c5cf
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-08 10:14:52 +0200

    Add files via upload
    the function compiles on my end Output : f,c,i,o,n,t,

--- task 10.py
+++ task 10.py
@@ -3,5 +3,5 @@ def commonletter(firstword,secondword):
   for letter in words:
     print(letter,end = ",")
 commonletter("fraction","function")
-
+#hi the code compiles on my end Output : f,c,i,o,n,t,
 
