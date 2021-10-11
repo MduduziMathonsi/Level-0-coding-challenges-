def1afc67db49ded83b2f4154efa5e289a945dd2
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2021-10-11 15:04:44 +0200

    Add files via upload

--- task 0.8.py
+++ task 0.8.py
@@ -2,13 +2,15 @@ def hoursminutes(number):
  convnumber = int(number)
  h= int(convnumber / 60)
  m = int(convnumber-(1*60))
- if h<=1 and m<=1 :
+ if h<=0 and m<=0:
+  print(number,"minutes")
+ elif h<=1 and m<=1 :
   print(h,"hour :",m,"minute")
  elif h>=1 and m>=1 :
   print(h,"hours :",m,"minutes")
  else:
    print("Abort Mission Impossible :-)")
-hoursminutes(61)
+hoursminutes(59)
 
 
 
