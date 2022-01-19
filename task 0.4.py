48d694dfcd4ed68ca0661e8ab211c0f531c16cbf
Mduduzi Mathonsi <mduduzi.mathonsi@bcx.co.za>
2022-01-19 17:11:00 +0200

    Update task 0.4.py

--- task 0.4.py
+++ task 0.4.py
@@ -1,8 +1,8 @@
-def evenOrOdd(number): #should not ask the user for input hence function parameter 
+def even_or_odd(number): 
  check = number % 2 
  if check > 0:
     print("odd")
  else:
     print("even")
-evenOrOdd(77) #function to print odd or even hence called a function with an arg
+even_or_odd(77) 
     
